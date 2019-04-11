873cdaffb6d4bc2c345a846ebc4c009f83775cb1
Joao Antunes <joao.antunes@tagus.ist.utl.pt>
2019-04-11 19:35:09 +0200

    Fix to the __init__ s

--- rmail_api/api/__init__.py
+++ rmail_api/api/__init__.py
@@ -3,6 +3,6 @@ from __future__ import absolute_import
 # flake8: noqa
 
 # import apis into api package
-from api.manifest_api import ManifestApi
-from api.shipment_api import ShipmentApi
-from api.token_api import TokenApi
+from rmail_api.api.manifest_api import ManifestApi
+from rmail_api.api.shipment_api import ShipmentApi
+from rmail_api.api.token_api import TokenApi
